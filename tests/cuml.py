d4e4a40ed2bbee37a83f082b6d482da265baf15a
harry1576 <harrydobbs87@gmail.com>
2023-08-29 16:48:41 +1200

    WIP

---
+++ tests/cuml.py
@@ -0,0 +1,30 @@
+import cupy as cp
+import cuml
+from cuml.neighbors import NearestNeighbors
+
+# Define the number of points
+N = 500000
+M = 500000
+
+# Generate some random Nx3 and Mx3 points on the GPU
+points1 = cp.random.rand(N, 3)
+points2 = cp.random.rand(M, 3)
+
+# Define the number of nearest neighbors to find
+k = 1
+
+# Initialize the NearestNeighbors model
+nn = NearestNeighbors(n_neighbors=k, metric="euclidean")
+
+# Fit the model to points2 (build KD-tree on the GPU)
+nn.fit(points2)
+
+# Perform the nearest neighbor search for points1 on the GPU
+distances, indices = nn.kneighbors(points1)
+
+# 'distances' contains the distances from each point in points1 to its nearest neighbor in points2
+# 'indices' contains the indices of the nearest neighbors in points2
+
+# Print the results
+print("Distances:", distances)
+print("Indices:", indices)
---
+++ tests/point_to_point_kd.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+from sklearn.neighbors import KDTree
+
+# Define the number of points
+N = 500000
+M = 500000
+
+# Define the batch size for splitting
+batch_size = 30000
+
+# Generate some random Nx3 and Mx3 points
+points1 = torch.rand(N, 3)  # Nx3 tensor
+points2 = torch.rand(M, 3)  # Mx3 tensor
+
+# Initialize an empty tensor to store distances
+
+# Build KD-tree for points2
+points2_np = points2.numpy()
+kdtree = KDTree(points2_np)
+
+# Loop through mini-batches
+for i in range(0, N, batch_size):
+    batch_points1 = points1[i : i + batch_size]
+
+    # Query the KD-tree for each batch of points
+    distances_batch, _ = kdtree.query(batch_points1.numpy(), k=batch_size)
+
+    # Store the results in the distances tensor
+
+# 'distances' now contains the pairwise distances for all points
+print(distances)
