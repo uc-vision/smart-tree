85a0d4c42a330d811f48d70764768d64fabc41b9
harry1576 <harrydobbs87@gmail.com>
2023-08-29 16:41:41 +1200

    okay

---
+++ tests/point_to_point.py
@@ -0,0 +1,22 @@
+import torch
+
+# Define the number of points
+N = 500000
+M = 500000
+
+# Define the batch size for splitting
+batch_size = 2000
+
+# Generate some random Nx3 and Mx3 points
+points1 = torch.rand(N, 3)  # Nx3 tensor
+points2 = torch.rand(M, 3).cuda()  # Mx3 tensor
+
+
+# Loop through mini-batches
+for i in range(0, N, batch_size):
+    batch_points1 = points1[i : i + batch_size]
+
+    # Compute pairwise distances for the current mini-batch
+    batch_distances = torch.norm(
+        batch_points1[:, None, :].cuda() - points2[None, :, :], dim=2
+    )
