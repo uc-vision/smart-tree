82fb5d7215b47ec4c9115e2bbbb5110a1248a815
Harry <harrydobbs87@gmail.com>
2023-08-30 15:12:38 +1200

    Update create-env.sh
    Remove mamba from the create-env script

--- create-env.sh
+++ create-env.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 
-conda install mamba -n base -c conda-forge
-mamba env create --file enviroment.yml
-
-mamba run -n smart-tree pip install -e .
+conda env create --file enviroment.yml
+conda run -n smart-tree pip install -e .
 
 git clone --recursive https://github.com/lxxue/FRNN.git
-mamba run -n smart-tree pip install -e FRNN/external/prefix_sum/.
-mamba run -n smart-tree pip install -e FRNN/.
+conda run -n smart-tree pip install -e FRNN/external/prefix_sum/.
+conda run -n smart-tree pip install -e FRNN/.
