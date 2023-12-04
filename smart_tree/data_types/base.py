start to add in another augmentation, there is a bug in masking, point cloud logging is okay

smart_tree/data_types/base.py
@@ -23,7 +23,7 @@ class Base:
             for k, v in asdict(self).items()
         }
 
-    def filter(self, mask: TensorType["N", torch.bool]):
+    def filter(self, mask: TensorType["N"]):
         filtered_args = self.apply_to_2d_tensors(lambda v: v[mask])
         return self.__class__(**filtered_args)
 

smart_tree/data_types/cloud.py
@@ -22,7 +22,7 @@ patch_typeguard()
 @typechecked
 @dataclass
 class Cloud(Base):
-    xyz: TensorType["N", 3]
+    xyz: TensorType["N", 3, torch.float32]
     rgb: Optional[TensorType["N", 3]] = None
     filename: Optional[Path] = None
 
@@ -48,7 +48,10 @@ class Cloud(Base):
         return self.__class__(xyz=translated_xyz, rgb=self.rgb, filename=self.filename)
 
     def rotate(self, rotation_matrix: TensorType[3, 3, float]) -> Cloud:
-        rotated_xyz = torch.matmul(self.xyz, rotation_matrix.T.to(self.xyz.device))
+        rotated_xyz = torch.matmul(self.xyz, rotation_matrix.T.to(self.xyz.device)).to(
+            torch.float32
+        )
+
         return self.__class__(xyz=rotated_xyz, rgb=self.rgb, filename=self.filename)
 
     def voxel_downsample(self, voxel_size: float | TensorType[1]) -> Cloud:
@@ -145,7 +148,9 @@ class LabelledCloud(Cloud):
         if self.loss_mask == None:
             mask_shape = (len(self), 1)
             self.loss_mask = torch.ones(
-                mask_shape, device=self.device, dtype=torch.bool
+                mask_shape,
+                device=self.device,
+                dtype=torch.bool,
             )
 
         self.point_ids = torch.arange(
@@ -179,9 +184,9 @@ class LabelledCloud(Cloud):
 
     def rotate(self, rot_matrix: TensorType[3, 3]) -> LabelledCloud:
         args = asdict(self)
-        rot_mat = rot_matrix.T.to(self.xyz.device)
+        rot_mat = rot_matrix.T.to(self.device).to(self.xyz.dtype)
 
-        args["xyz"] = torch.matmul(args["xyz"], rot_mat)
+        args["xyz"] = torch.matmul(args["xyz"], rot_mat).to(self.xyz.dtype)
         if args["medial_vector"] is not None:
             args["medial_vector"] = torch.matmul(args["medial_vector"], rot_mat)
         if args["branch_direction"] is not None:
@@ -194,6 +199,18 @@ class LabelledCloud(Cloud):
     def filter_by_class(self, classes: TensorType["N"]) -> LabelledCloud:
         mask = torch.isin(self.class_l, classes)
         return self.filter(mask.view(-1))
+
+    def add_xyz(self, xyz, class_l=None):
+        self.xyz = torch.cat((self.xyz, xyz))
+        padd_vector = torch.zeros(
+            (xyz.shape[0], 3),
+            device=self.device,
+            dtype=self.xyz.dtype,
+        )
+
+        self.medial_vector = torch.cat((self.medial_vector, padd_vector))
+        if class_l != None:
+            self.class_l = torch.cat((self.class_l, class_l))
 
     @property
     def number_classes(self) -> int:

smart_tree/dataset/augmentations.py
@@ -112,7 +112,7 @@ class RandomCubicCrop(Augmentation):
         return cloud.filter(mask)
 
 
-class RandomGaussianNoise(Augmentation):
+class RandomGaussianPeturb(Augmentation):
     def __init__(self, mean=0.0, std=0.01, prob=0.0, magnitude=1.0):
         self.mean = mean
         self.std = std
@@ -122,19 +122,45 @@ class RandomGaussianNoise(Augmentation):
     def __call__(self, cloud):
         mask = torch.rand(cloud.xyz.shape[0]) < self.prob
         noise = (
-            torch.randn(cloud.xyz.shape, device=cloud.xyz.device) * self.std + self.mean
+            torch.randn(cloud.xyz.shape, device=cloud.xyz.device, dtype=torch.float32)
+            * self.std
+            + self.mean
         ).float()
         noise *= self.magnitude
         cloud.xyz[mask] += noise[mask]
         return cloud.with_xyz(cloud.xyz)
 
 
-class RandomRotate(Augmentation):
-    def __init__(self, max_x, max_y, max_z):
-        self.max_rots = torch.tensor([max_x, max_y, max_z])
+class SaltNoise(Augmentation):
+    def __init__(self, percentage=0.0, label_id=5):
+        self.percentage = percentage  # based on number of xyz points add random points
+        self.label_id = label_id
 
     def __call__(self, cloud):
-        x, y, z = (torch.rand(3) - 0.5) * self.max_rots
+        num_points = int(len(cloud) * self.percentage)
+
+        min_xyz = cloud.min_xyz
+        max_xyz = cloud.max_xyz
+
+        points = (
+            torch.rand((num_points, 3), dtype=torch.float32, device=cloud.device)
+            * (max_xyz - min_xyz)
+            + min_xyz
+        )
+        labels = torch.full((num_points, 1), self.label_id, device=cloud.device).float()
+
+        print(points)
+        print(labels)
+        cloud.add_xyz(points, labels.to(cloud.device))
+        return cloud
+
+
+class RandomRotate(Augmentation):
+    def __init__(self, max_x, max_y, max_z):
+        self.max_rots = torch.tensor([max_x, max_y, max_z], dtype=torch.float32)
+
+    def __call__(self, cloud):
+        x, y, z = (torch.rand(3, dtype=torch.float32) - 0.5) * self.max_rots
 
         self.rot_mat = euler_angles_to_rotation(
             torch.tensor([x, y, z], device=cloud.xyz.device)

smart_tree/model/sparse/model.py
@@ -97,14 +97,14 @@ class Smart_Tree(nn.Module):
         self,
         predictions: dict,
         targets: torch.tensor,
-        mask: torch.tensor,
+        meta_data: torch.tensor,
     ):
         losses = {}
 
-        mask = mask.reshape(-1).long()
+        mask = meta_data.mask.reshape(-1).bool()
 
         if "radius" in self.target_features:
-            target_radius = targets["radius"][mask]
+            target_radius = targets["radius"]  # [mask]
             if self.log_radius:
                 target_radius = torch.log(target_radius)
             pred_radius = predictions["radius"][mask]
@@ -116,8 +116,8 @@ class Smart_Tree(nn.Module):
             losses["medial_direction"] = self.direction_loss(pred_dir, tgt_dir)
 
         if "class_l" in self.target_features:
-            target_class = targets["class_l"][mask]
-            pred_class = predictions["class_l"][mask]
+            target_class = targets["class_l"]  # [mask]
+            pred_class = predictions["class_l"]  # [mask]
             losses["class_l"] = self.class_loss(pred_class, target_class)
 
         return losses

smart_tree/model/sparse/transform.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Optional
 
 import torch
 from spconv.pytorch import SparseConvTensor
@@ -13,10 +13,10 @@ class VoxelizedTrainingData:
     input_voxelized: Dict[str, torch.Tensor]
     coords: torch.Tensor
     targets: Dict[str, torch.Tensor]
-    point_id: torch.Tensor
     voxel_id: torch.Tensor
-    mask: torch.Tensor
-    filename: str
+    filename: str | List[str]
+    mask: Optional[torch.Tensor] = None
+    point_id: Optional[torch.Tensor] = None
 
 
 def merge_voxelized_data(
@@ -25,15 +25,27 @@ def merge_voxelized_data(
     merged_input_voxelized = {}
     merged_targets = {}
 
+    # Initialize lists to store point_ids and masks if they exist
+    point_ids = []
+    masks = []
+    file_names = []
+
     for i, data in enumerate(data_list):
         data.coords[:, 0] = torch.tensor([i], dtype=torch.float32)
-        data.point_id[:, 0] = torch.tensor([i], dtype=torch.float32)
+
+        if data.point_id is not None:
+            data.point_id[:, 0] = torch.tensor([i], dtype=torch.float32)
+            point_ids.append(data.point_id)
+
+        if data.mask is not None:
+            masks.append(data.mask)
 
     merged_coords = torch.cat([data.coords for data in data_list], dim=0)
-    merged_point_id = torch.cat([data.point_id for data in data_list], dim=0)
-
     merged_voxel_id = torch.cat([data.voxel_id for data in data_list], dim=0)
-    merged_mask = torch.cat([data.mask for data in data_list], dim=0)
+
+    # Merge point_ids and masks if they are not empty
+    merged_point_id = torch.cat(point_ids, dim=0) if point_ids else None
+    merged_mask = torch.cat(masks, dim=0) if masks else None
 
     for data in data_list:
         for key, tensor in data.input_voxelized.items():
@@ -50,6 +62,8 @@ def merge_voxelized_data(
             else:
                 merged_targets[key] = tensor.clone()
 
+        file_names.append(data.filename)
+
     return VoxelizedTrainingData(
         input_voxelized=merged_input_voxelized,
         coords=merged_coords,
@@ -57,7 +71,7 @@ def merge_voxelized_data(
         point_id=merged_point_id,
         voxel_id=merged_voxel_id,
         mask=merged_mask,
-        filename=data_list[0].filename,
+        filename=file_names,  # Assuming all data have the same filename
     )
 
 
@@ -101,10 +115,12 @@ def concated_tensor_to_dict(tensor: torch.tensor, feats_shape: Dict):
 
 
 def sparse_voxelize(
-    cld: Union[Cloud, LabelledCloud],
+    cld: Cloud | LabelledCloud,
     voxel_size: float,
     input_feature_names: List[str],
-    target_feature_names: List[str],
+    target_feature_names: List[str] = [],
+    extract_point_ids=True,
+    extract_masks=True,
 ) -> Dict:
     all_feats_dict = extract_cloud_features(
         cld, input_feature_names + target_feature_names
@@ -113,8 +129,16 @@ def sparse_voxelize(
     all_feats_tensor = dict_to_concated_tensor(all_feats_dict)
 
     target_feats = extract_cloud_features(cld, target_feature_names)
-    mask = list(extract_cloud_features(cld, ["loss_mask"]).values())[0]
-    point_id = list(extract_cloud_features(cld, ["point_ids"]).values())[0]
+
+    if extract_masks:
+        mask = list(extract_cloud_features(cld, ["loss_mask"]).values())[0]
+    else:
+        mask = None
+
+    if extract_point_ids:
+        point_id = list(extract_cloud_features(cld, ["point_ids"]).values())[0]
+    else:
+        point_id = None
 
     voxel_gen = PointToVoxel(
         vsize_xyz=[voxel_size] * 3,
@@ -143,8 +167,8 @@ def sparse_voxelize(
         input_feats,
         coords,
         target_feats,
-        point_id,
         voxel_id,
-        mask,
         cld.filename,
+        mask,
+        point_id,
     )

smart_tree/model/train.py
@@ -18,14 +18,14 @@ from smart_tree.data_types.cloud import LabelledCloud
 from .helper import log_cloud_on_wandb
 
 
-def train_epoch(data_loader, model, optimizer, fp16=False, scaler=None, tracker=None):
+def train_epoch(data_loader, model, optimizer, scaler=None, tracker=None):
     data_loader_tqdm = tqdm(data_loader, desc="Training", leave=False)
 
     for model_input, targets, meta_data in data_loader_tqdm:
         preds = model.forward(model_input)
         loss = model.compute_loss(preds, targets, meta_data)
 
-        if fp16:
+        if scaler:
             assert sum(loss.values()).dtype is torch.float32
             scaler.scale(sum(loss.values())).backward()
             scaler.step(optimizer)
@@ -129,7 +129,7 @@ def main(cfg: DictConfig):
 
     # FP-16
     amp_ctx = torch.cuda.amp.autocast() if cfg.fp16 else contextlib.nullcontext()
-    scaler = torch.cuda.amp.grad_scaler.GradScaler()
+    scaler = torch.cuda.amp.grad_scaler.GradScaler() if cfg.fp16 else None
 
     epochs_no_improve = 0
     best_epoch_loss = torch.inf
@@ -142,16 +142,11 @@ def main(cfg: DictConfig):
                 model,
                 optimizer,
                 scaler=scaler,
-                fp16=cfg.fp16,
                 tracker=training_tracker,
             )
 
             val_tracker = eval_epoch(val_loader, model, val_tracker)
             test_tracker = eval_epoch(test_loader, model, test_tracker)
-
-        if cfg.capture_output and (epoch + 1) % cfg.capture_epoch == 0:
-            clouds: List = capture_clouds(capturer_loader, model, capture_function)
-            log_cloud_on_wandb(clouds, epoch)
 
         if val_tracker.epoch_loss < best_epoch_loss:
             epochs_no_improve = 0
@@ -161,6 +156,12 @@ def main(cfg: DictConfig):
             log.info(f"Weights Saved at epoch: {epoch}")
         else:
             epochs_no_improve += 1
+
+        if (
+            cfg.capture_output and (epoch + 1) % cfg.capture_epoch == 0
+        ) or epochs_no_improve == 0:
+            clouds: List = capture_clouds(capturer_loader, model, capture_function)
+            log_cloud_on_wandb(clouds, epoch)
 
         scheduler.step(val_tracker.epoch_loss) if cfg.lr_decay else None
 