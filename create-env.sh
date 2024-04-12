028b2213c37ab5049f86a008cee7c4a5c085ba76
Harry <harrydobbs87@gmail.com>
2024-04-13 11:34:34 +1200

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -2,6 +2,7 @@
 eval "$(conda shell.bash hook)"
 
 conda create -n smart-tree python=3.10
+conda run -n smart-tree conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 conda run -n smart-tree pip install -e .
 
 echo Installing FRNN
