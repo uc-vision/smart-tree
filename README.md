014aa78eb668e2252139dad63dd8f2ed27beea6d
harry <harrydobbs87@gmail.com>
2024-04-13 20:31:22 +1200

    updated install script

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Please follow instructions to download data from this <a href="https://github.co
 ## 🔧 Installation:
 
 First, make sure you have Conda installed.
+We recommend you install <a href="https://www.anaconda.com/blog/a-faster-conda-for-a-growing-community">mamba solver</a >.
 To install smart-tree please use <br>`bash create-env.sh`<br>
 Then activate the environment using: <br>`conda activate smart-tree`
 
--- create-env.sh
+++ create-env.sh
@@ -2,6 +2,7 @@
 eval "$(conda shell.bash hook)"
 
 conda create -n smart-tree python=3.10
+conda run -n smart-tree conda install -c "nvidia/label/cuda-11.8.0" cuda-toolkit
 conda run -n smart-tree conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 conda run -n smart-tree pip install -e .
 
@@ -9,5 +10,4 @@ echo Installing FRNN
 git clone --recursive https://github.com/lxxue/FRNN.git
 conda run -n smart-tree pip install -e FRNN/external/prefix_sum/.
 conda run -n smart-tree pip install -e FRNN/.
-
-conda run -n smart-tree conda install cudf=24.02 cugraph=24.02 -c rapidsai -c conda-forge -c nvidia
+conda run -n smart-tree conda install -c rapidsai -c conda-forge -c nvidia rapids=24.04 cuda-version=11.8
