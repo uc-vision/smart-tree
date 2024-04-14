12000a861b7c82f267fa82900458196e3ce52470
harry <harrydobbs87@gmail.com>
2024-04-14 14:48:11 +1200

    fixed enviroment install script

--- create-env.sh
+++ create-env.sh
@@ -5,7 +5,7 @@ conda create -n smart-tree python=3.10 &&
 conda run -n smart-tree conda install -c "nvidia/label/cuda-11.8.0" cuda-toolkit &&
 conda run -n smart-tree conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia &&
 conda run -n smart-tree conda install gxx_linux-64 && # For installing FRNN &&
-conda run -n smart-tree pip install . &&
+conda run -n smart-tree pip install -e . &&
 
 echo Installing FRNN
 git clone --recursive https://github.com/lxxue/FRNN.git 
